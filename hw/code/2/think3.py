70833e05ebf553a3f6d53b31850221319b8d28ec
sganesh4 <sushil.89@gmail.com>
2015-08-27 14:13:14 +0000

    Message: Completed exercise 3.5 Description: Updated file think3.py with part 2of exercise 3

--- hw/code/2/think3.py
+++ hw/code/2/think3.py
@@ -1,9 +1,10 @@
 
 
 #hw2 exercise 3.2
-def exercise3_5():
-    for i in range(11): 
-        for j in range(11):
+def exercise3_5(n):
+    print 'Number of rows and columns: ', n
+    for i in range((n-1)*5+1): 
+        for j in range((n-1)*5+1):
 # Learned something new here bitwise-and is '&' while boolean is (and) 
             if i%5==0 and j%5==0:
                 print '+',
@@ -17,4 +18,5 @@ def exercise3_5():
 
 # Main function. Put functions you want to all when script runs here
 if __name__ == '__main__':
-    exercise3_5()
+    exercise3_5(3)
+    exercise3_5(4)
