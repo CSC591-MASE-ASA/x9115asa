c9da5239d52fc1406b601d9a082821edd5449385
Aniket Lawande <anlawande@gmail.com>
2015-11-04 10:43:10 -0500

    Code 9 - Basic skeleton

---
+++ hw/code/9/code9.py
@@ -0,0 +1,37 @@
+fitness_family = "Something"
+
+class candidate:
+    fitness = 0
+    decs = []
+    def __init__(self, decs):
+        self.decs = decs
+        calc_fitness()
+
+    def calc_fitness():
+        return
+
+class population:
+    candidates = []
+    fitness_family = "Something"
+
+    def __init__(self, fitness_family):
+        self.fitness_family = fitness_family
+
+    def crossover(self, candidate1, candidate2):
+        return
+    def select(self):
+        return
+    def mutate(self, candidate, probability):
+        return
+
+class GA:
+    generations = []
+
+    def __init__(self, fitness_family):
+        return
+
+    def GA(self, dtlz_id, num_opts, num_decs):
+        return
+
+ga = GA("")
+ga.GA(1, 2, 20)
