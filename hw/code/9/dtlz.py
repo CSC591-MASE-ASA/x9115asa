852b5d0a87d5bc7fc28eecae194181bcab683dfd
Aniket Lawande <anlawande@gmail.com>
2015-11-04 22:56:52 -0500

    code 9 - dtlz3

--- hw/code/9/dtlz.py
+++ hw/code/9/dtlz.py
@@ -24,3 +24,28 @@ def dtlz1(x, num_objs, num_decs):
     f[num_objs-1] = 0.5 * (1 - x[0]) * (1 + g)
 
     return f
+
+def dtlz3(x, num_objs, num_decs):
+    f = [None]*num_objs
+
+    def gx():
+        g = 0.0
+        for i in range(0, num_decs-1):
+            g += math.pow(x[i] - 0.5, 2) - math.cos(20* math.pi * (x[i] - 0.5))
+        g = 100 * (g + num_decs)
+        return g
+
+    g = gx()
+    f[0] = 1 + g
+    for i in range(0, num_objs-2):
+        f[0] *= math.cos(x[i]*math.pi/2)
+
+    for i in range(1, num_objs - 1):
+        f[i] = 1 + g
+        for j in range(0, num_objs - (i+1)):
+            f[i] *= math.cos(x[j]*math.pi/2)
+        f[i] *= math.sin(x[num_objs - (i+1)] * math.pi/2)
+
+    f[num_objs-1] = (1 + g) * math.sin(x[0] * math.pi/2)
+
+    return f
