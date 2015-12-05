21dc7c26a628d33b34d7a00d914de8388eb0d180
Sushil Ganesh <sganesh4@ncsu.edu>
2015-12-04 23:17:42 -0500

    Code 10 ready for DE runs

--- hw/CODE10/code10.py
+++ hw/CODE10/code10.py
@@ -2,28 +2,35 @@ from __future__ import division
 __author__ = 'Sushil'
 from models import Model
 import dtlz
+from code9 import *
 import random
 
 
 class Tuner_Model(Model):
-    def __init__(self, lower=[0.01, 50, 100], upper = [0.2, 150, 1000], algo_model=):
+    def __init__(self, lower=[0.01, 50, 100], upper = [0.2, 150, 1000], algo_model=dtlz.dtlz1, algo_num_obj=1, algo_num_decs=1):
         Model.__init__(self)
         #define upper and lower bounds for dec variables: Mutation rate, no of candidates and no of gens
         self.name = 'Tuner Model'
         self.lower_bounds = lower
         self.upper_bounds = upper
         self.no_of_decisions = len(lower)
+        self.algo_model = algo_model
+        self.algo_num_obj = algo_num_obj
+        self.algo_num_decs = algo_num_decs
+    def energy(self, s):
+        ga = GA(self.algo_model, self.algo_num_obj, self.algo_num_decs, *s)
+        hve1 = ga.run()
+        return hve1.hyper_vol
 
 
-def differential_evolution(self, model = Tuner_Model):
-        era_length = 100
-        seed = [0.05, 100, 500]
+
+
+def differential_evolution(model = Tuner_Model()):
         cr = 0.3
         f = 0.5
-        kmax=1000
-        np = era_length
-        eras = 5
-        k = 0
+        kmax=5
+        np = 10
+        seed = model.get_decision()
         def mutate(these):
             sn=[]
             def smear(vals, idx):
@@ -52,34 +59,38 @@ def differential_evolution(self, model = Tuner_Model):
                   x = lst[  int(random.uniform(0,len(lst))) ]
                 seen.append( id(x) )
                 return x
-
             assert len(lst) > 4
             avoid = avoid or lst[0]
             seen  = [ id(avoid) ]
             return unique_item(), unique_item(), unique_item()
         frontier = create_frontier(seed)
         sb = seed
-        e = eb = model.normalize(model.energy(seed))
-        while k < kmax :
+        eb = model.energy(seed)
+        print 'Best energy so far: '+str(eb)
+        print 'Best solution so far: '+str(sb)
+        for k in xrange(kmax):
             for i,candidate in enumerate(frontier):
-                e = model.normalize(model.energy(candidate))
+                e = model.energy(candidate)
                 x, y, z = generate_items(frontier)
-                sn=x
                 sn = mutate((x,y,z)) #mutate function
-                en = model.normalize(model.energy(sn))
-                if self.model.eval(sn) and en < e:
+                en = model.energy(sn)
+                if en > e:
                     frontier[i] = sn
-                if model.eval(sn) and en < eb:
+                if en > eb:
                     sb, eb = sn, en
-                    out='!'
-                op+=out
-                k = k + 1
+                print 'Best energy so far: '+str(eb)
+                print 'Best solution so far: '+str(sb)
         return sb
 
 
 def main():
     seed=[]
     #insert initial call to DE to tune GA here
+    #define upper/lower limits for mutation rate, no candidates and no gens
+    lower = [0.01, 50, 500]
+    upper = [0.3, 150, 1000]
+    tm = Tuner_Model(lower, upper, dtlz.dtlz1, 2, 10)
+    print differential_evolution(tm)
 
 if __name__ == "__main__":
     main()
